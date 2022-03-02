1a5349b9d8e087cd6807734d2369e46a90f48040
mithun <mithunrptc@gmail.com>
2022-03-02 10:55:53 +0600

    Admin & non amdin users management browser test complete

--- tests/Browser/AuthenticationTest.php
+++ tests/Browser/AuthenticationTest.php
@@ -2,12 +2,32 @@
 
 namespace Tests\Browser;
 
+use App\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 
 class AuthenticationTest extends DuskTestCase
 {
+    // use DatabaseMigrations;
+
+    protected $user;
+    protected $amdinUser;
+    protected $nonAdminUser;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->artisan('migrate:fresh');
+        $this->artisan('db:seed');
+
+        $this->user = User::factory()->create();
+        $this->amdinUser = User::factory()->admin_user()->create();
+        $this->nonAdminUser = User::factory()->non_admin_user()->create();
+    }
+
+
+
     /**
      * A Dusk test example.
      *
@@ -24,6 +44,7 @@ class AuthenticationTest extends DuskTestCase
                     ->assertPathIs('/admin/companies')
                     ->assertSee('Company List')
                     ->assertAuthenticated()
+                    ->logout()
                     ;
         });
     }
@@ -36,13 +57,13 @@ class AuthenticationTest extends DuskTestCase
      */
     public function test_asserts_that_user_can_logged_out()
     {
-        $this->browse(function (Browser $browser) {
-            $browser->visit('/login')
-                    ->type('email', 'admin@admin.com')
-                    ->type('password', 'password')
-                    ->press('Login')
+        $user = $this->user;
+        $this->browse(function (Browser $browser) use($user) {
+            $browser->loginAs($user)
+                    ->visit('/admin')
                     ->clickLink('Logout')
                     ->assertGuest()
+                    ->logout()
                     ;
         });
     }
@@ -63,9 +84,50 @@ class AuthenticationTest extends DuskTestCase
                     ->assertPathIs('/login')
                     ->assertSee('These credentials do not match our records.')
                     ->assertElementHasClass('input[name="email"]', 'is-invalid')
+                    ->logout()
                     ;
             // $classLists = $browser->attribute('input[name="email"]', 'class');
             // $this->assertTrue(in_array('is-invalid', explode(' ', $classLists)));
+        });
+    }
+
+
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function test_admin_users_can_manage_users_manage()
+    {
+        $user = User::factory()->admin_user()->create();
+        $this->browse(function (Browser $browser) use($user) {
+            $browser->loginAs($user)
+                    ->visit('/admin')
+                    ->assertSee('User management')
+                    ->visit('/admin/users')
+                    ->assertSee('User List')
+                    ->logout()
+                    ;
+        });
+    }
+
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function test_admin_users_can_not_manage_users_manage()
+    {
+        $user = User::factory()->non_admin_user()->create();
+        $this->browse(function (Browser $browser) use($user) {
+            $browser->loginAs($user)
+                    ->visit('/admin')
+                    ->assertDontSee('User management')
+                    ->visit('/admin/users')
+                    // ->pause(5000)
+                    ->assertSee('FORBIDDEN')
+                    ->logout()
+                    ;
         });
     }
 }
